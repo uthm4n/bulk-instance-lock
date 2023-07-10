2ebd44df11126831a0fc0c22a837ebdb15f0dac8
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-07-10 12:39:25 +0100

    Create bulk-instance-lock.py

---
+++ bulk-instance-lock.py
@@ -0,0 +1,27 @@
+import requests
+import json 
+
+
+url = "https://<YOUR-MORPHEUS-APPLIANCE-URL>/api/instances?max=25&offset=0&showDeleted=false&details=false"  # change the appliance URL 
+
+headers = {
+    "accept": "application/json",
+    "authorization": "Bearer <YOUR-MORPHEUS-API-TOKEN>"      # add your Morpheus API token 
+}
+
+response = requests.get(url, headers=headers, verify=False)
+data = response.json()
+instanceIDs = []
+for IDs in data['instances']:
+    IDs = IDs['id']
+    instanceIDs.append(IDs)
+
+
+for h in instanceIDs: 
+    url = f"https://<YOUR-MORPHEUS-APPLIANCE-URL>/api/instances/{h}/lock"
+    headers = {
+    "accept": "application/json",
+    "authorization": "Bearer <YOUR-MORPHEUS-API-TOKEN>"
+    }
+    response = requests.put(url, headers=headers, verify=False)
+    print(response.text)
